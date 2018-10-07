983e11843e6d2cc44692dbc5cb3e3ec9609189a1
Ryan McCauley <ryanmccauley211@gmail.com>
2018-10-07 19:55:26 +0100

    - Change base text class from Text to Canvas for flexibility - Use font metrics to offset position and autosize canvas

--- src/main/java/TextJX.java
+++ src/main/java/TextJX.java
@@ -1,30 +1,38 @@
+import com.sun.javafx.tk.FontMetrics;
+import com.sun.javafx.tk.Toolkit;
+import javafx.geometry.VPos;
 import javafx.scene.Node;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.PixelReader;
+import javafx.scene.image.WritableImage;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
 
 public class TextJX implements ObjJX {
 
-    private VBox wrapper;
-    private Text text;
+    private Canvas dynamicCanvas;
+    private GraphicsContext gfx;
 
     private TextJX(){}
 
     public static TextJX SimpleText(String text){
         TextJX textJX = new TextJX();
-        textJX.text = new Text(0, 0, text);
-        textJX.text.setFont(new Font(15));
-        textJX.text.setFill(Color.BLUE);
-        textJX.wrapper = new VBox(textJX.text);
+        textJX.dynamicCanvas = new DynamicCanvas(0, 0);
+        textJX.gfx = textJX.dynamicCanvas.getGraphicsContext2D();
+        textJX.gfx.setStroke(Color.BLACK);
+
+        FontMetrics fm = Toolkit.getToolkit().getFontLoader().getFontMetrics(textJX.gfx.getFont());
+        textJX.dynamicCanvas.setWidth(fm.computeStringWidth("Hello World"));
+        textJX.dynamicCanvas.setHeight(fm.getAscent() - fm.getDescent() + textJX.gfx.getLineWidth());
+        textJX.gfx.strokeText(text, 0, fm.getAscent() - fm.getDescent());
         return textJX;
     }
 
-    public VBox getText() {
-        return wrapper;
-    }
-
     public Node getNode() {
-        return wrapper;
+        return dynamicCanvas;
     }
 }
