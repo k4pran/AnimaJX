8553ba68ae4f98657d3ccaf88aec38295dd436bb
Ryan McCauley <ryanmccauley211@gmail.com>
2018-10-07 19:51:38 +0100

    - Add point setters

--- src/main/java/Point.java
+++ src/main/java/Point.java
@@ -15,4 +15,12 @@ public class Point {
     public double getY() {
         return y;
     }
+
+    public void setX(double x) {
+        this.x = x;
+    }
+
+    public void setY(double y) {
+        this.y = y;
+    }
 }
