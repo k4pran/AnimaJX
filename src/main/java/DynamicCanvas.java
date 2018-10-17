7fb82366401a4f1fd966918da5c4ce63f4c20504
Ryan McCauley <ryanmccauley211@gmail.com>
2018-10-17 18:06:17 +0100

    - Add method for debugging to show canvas outline

--- src/main/java/DynamicCanvas.java
+++ src/main/java/DynamicCanvas.java
@@ -1,9 +1,20 @@
 import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.paint.Color;
 
 public class DynamicCanvas extends Canvas {
 
     public DynamicCanvas(double width, double height) {
         super(width, height);
+        SHOW_OUTLINE();
+    }
+
+    // DEBUGGING ONLY
+    public void SHOW_OUTLINE() {
+        GraphicsContext g = this.getGraphicsContext2D();
+        g.setFill(new Color(0, 1, 0, 0.3));
+        g.fillRect(0, 0, getWidth(), getHeight());
+
     }
 
     @Override
